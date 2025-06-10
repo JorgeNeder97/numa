baaa520c8704b322de9d906d99d10413a831cf39
JorgeNeder97 <jorge.neder97@gmail.com>
2025-06-10 00:43:24 -0300

    Trabajando en Testimonios.tsx

--- src/components/Testimonios.tsx
+++ src/components/Testimonios.tsx
@@ -1,7 +1,18 @@
 const Testimonios = () => {
     return (
-        <div>
-            <span>Testimonios</span>
+        <div className="w-full flex flex-col place-items-center gap-[10vh]">
+            <div className="w-full flex flex-col place-items-center gap-[20px]">
+                <div className="fromLeft w-[45%] self-start border-b-2 pt-[1px] border-primary"></div>
+                <div className="flex flex-col place-items-center place-content-center gap-[10px] text-primary">
+                    <h3 className="fromRight font-bold text-[2rem]">Resultados con Numa</h3>
+                    <span className="fromLeft font-light text-[1rem]">Transformamos su forma de ver la plata</span>
+                </div>
+                <div className="fromRight w-[45%] self-end border-b-2 pt-[1px] border-primary"></div>
+            </div>
+
+            <div>
+                
+            </div>
         </div>
     );
 };
