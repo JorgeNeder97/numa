17059eef472c650ec1f5815fa95b0ea15418ea6c
JorgeNeder97 <jorge.neder97@gmail.com>
2025-10-18 18:27:20 -0300

    Fix: cambio en el script de build, cambio en el archivo de prisma.ts para evitar que se instancie mas de una vez

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,3 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+// Definimos un "global" que permita reutilizar la instancia en desarrollo
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+// Si ya existe una instancia global la usa, sino crea una nueva.
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"], // log útil durante desarrollo (es opcional)
+  });
+
+// En desarrollo guardamos la instancia en globalThis
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
